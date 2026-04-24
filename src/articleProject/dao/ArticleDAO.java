870006e44c9ff0b53dacdcbb6d909563bd6ebb7a
zmxn9941@naver.com <zmxn9941@naver.com>
2026-04-24 14:11:41 +0900

    feature : dao 시작

--- src/articleProject/dao/ArticleDAO.java
+++ src/articleProject/dao/ArticleDAO.java
@@ -1,4 +1,57 @@
 package articleProject.dao;
 
-public class ArticleDAO {
+import articleProject.Entity.Article;
+import articleProject.Entity.Comment;
+import articleProject.crudInterface.CrudInterface;
+import articleProject.db.DBConn;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+public class ArticleDAO extends CrudInterface {
+    @Override
+    public List<Article> all() {
+        Connection conn = DBConn.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        return List.of();
+    }
+
+    @Override
+    public void newArticle(Article article) {
+
+    }
+
+    @Override
+    public Article detail(Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public void update(Article article) {
+
+    }
+
+    @Override
+    public void insertComment(Comment comment) {
+
+    }
+
+    @Override
+    public void updateComment(Comment comment) {
+
+    }
+
+    @Override
+    public void deleteComment(Long deleteCommentId) {
+
+    }
 }
