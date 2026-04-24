17fcc09899abd029dbe99b99658bce2fa282c430
yzihn0123 <zihoon0123@gmail.com>
2026-04-24 10:04:27 +0900

    feat : Article 작업 완료

--- src/articleProject/Entity/Article.java
+++ src/articleProject/Entity/Article.java
@@ -1,8 +1,21 @@
 package articleProject.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Article {
     private long id;
     private String name;
     private String title;
     private String content;
+
+    private List<Comment> commentList = new ArrayList<>();
+
+    public void addComments(Comment comment) {
+        commentList.add(comment);
+    }
+
+    public void setCommentList(List<Comment> comments) {
+        commentList = comments;
+    }
 }
