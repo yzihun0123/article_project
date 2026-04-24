0d7c7886d937daa2d1c39bc052c01047f941e027
yzihn0123 <zihoon0123@gmail.com>
2026-04-24 11:30:56 +0900

    feat : CrudInterface 설계 완료

--- src/articleProject/crudInterface/CrudInterface.java
+++ src/articleProject/crudInterface/CrudInterface.java
@@ -1,4 +1,20 @@
 package articleProject.crudInterface;
 
-public class CrudInterface {
+import articleProject.Entity.Article;
+import articleProject.Entity.Comment;
+
+import java.util.List;
+
+public interface CrudInterface {
+    // 게시글
+    List<Article> all();
+    void    newArticle(Article article);
+    Article detail(Long id);
+    boolean delete(Long id);
+    void    update(Article article);
+
+    // 댓글
+    void insertComment(Comment comment);
+    void updateComment(Comment comment);
+    void deleteComment(Long deleteCommentId);
 }
